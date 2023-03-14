ee9d94a0112feb42f729b46458772deeb1df307f
Lekoba838 <62040064+Lekoba838@users.noreply.github.com>
2023-03-14 21:19:48 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-const dictInput = require('./101-data').dict;
-const outDict = {};
-
-for (const key in dictInput) {
-  const ocurr = dictInput[key];
-  outDict[ocurr] = [];
-  for (const keys in dictInput) {
-    if (dictInput[keys] === ocurr) {
-      outDict[ocurr].push(keys);
-    }
-  }
-}
-console.log(outDict);
+const dict = require('./101-data').dict;
+console.log(Object.entries(dict).reduce(function (i, idx) {
+  i[idx[1]] = (i[idx[1]] || []).concat(idx[0]);
+  return i;
+}, {}));
